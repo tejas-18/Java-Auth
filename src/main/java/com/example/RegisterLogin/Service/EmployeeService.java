17c04cd7825a699b35aec743a763b81b600ed5c8
tejas-18 <131752453+tejas-18@users.noreply.github.com>
2023-11-13 18:21:39 +0530

    Created code for authentication in java

---
+++ src/main/java/com/example/RegisterLogin/Service/EmployeeService.java
@@ -0,0 +1,12 @@
+package com.example.RegisterLogin.Service;
+
+import com.example.RegisterLogin.Dto.EmployeeDTO;
+import com.example.RegisterLogin.Dto.LoginDTO;
+import com.example.RegisterLogin.response.LoginResponse;
+
+public interface EmployeeService {
+
+    String addEmployee(EmployeeDTO employeeDTO);
+
+    LoginResponse loginEmployee(LoginDTO loginDTO);
+}
